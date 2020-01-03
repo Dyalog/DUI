d4304426323d3c5656148d2ddbde2bbf51bef850
Michael Baas <mbaas@dyalog.com>
2020-01-03 11:22:51 +0100

    Removing Syncfusion 16.0.3

--- PlugIns/Syncfusion-16.3.0.21/assets/scripts/aurelia/es6/common/common.js
+++
@@ -1,4 +0,0 @@
-import {customAttribute, bindable, customElement, children, TemplatingEngine, inlineView} from 'aurelia-templating';
-import {inject} from 'aurelia-dependency-injection';
-
-export {customAttribute, bindable, inject, inlineView, customElement, children, TemplatingEngine};
