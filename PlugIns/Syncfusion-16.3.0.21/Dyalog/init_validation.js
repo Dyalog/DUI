9e7c3f5910e1ebcecaa024f5561af3a29b83688b
Michael Baas <mbaas@dyalog.com>
2020-01-03 09:29:48 +0100

    Removed validation from ejAutocomplete-Sample

--- PlugIns/Syncfusion-16.3.0.21/Dyalog/init_validation.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $.validator.setDefaults({
-        ignore: [],
-        errorClass: "e-validation-error",
-        errorPlacement: function(error, element) {
-            $(error).insertAfter(element.closest(".e-widget"));
-        }
-    });
-})
